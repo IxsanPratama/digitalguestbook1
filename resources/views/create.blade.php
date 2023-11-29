41126104f96eff917e04c6aa4a2d797e44bb74a3
ixsan pratama <rimanura4@gmail.com>
2023-11-29 21:05:35 +0700

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<body>
+    <h3>Create</h3>
+    <hr>
+    <p>
+        isi
+    </p>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Digital Guest Book</title>
+</head>
+<body>
+    <h3>Digital Guest Book</h3>
+    <hr>
+    <a href="http://">HOME</a> | <a href="http://">CREATE</a> | <a href="http://">UPDATE</a> | <a href="http://">DELETE</a>
+</body>
+</html>
